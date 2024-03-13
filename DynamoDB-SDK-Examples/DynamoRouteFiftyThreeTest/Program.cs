fda8ace2be0cf4c1bf513279e7daf392e49a35ca
Sam Gajjar <sgajjarhawk@gmail.com>
2024-03-13 09:41:45 -0400

    commit all changes

---
+++ DynamoDB-SDK-Examples/DynamoRouteFiftyThreeTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotnetSamples;
+
+namespace DynamoRouteFiftyThreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var testddb = new DotnetSamples.DynamoR53ConnectionTestClass();
+            //var task2 = testddb.GlobalTableQueryTestFunction2Async();
+            //task2.Wait();
+
+            //testddb.GlobalTableQueryTestFunction3();
+
+            var task3 = testddb.DynamoRoute53PutItemTest1();
+            task3.Wait();            
+        }
+    }
+}
